e9f8494237f36992a72dc914d432ccf66b8d1431
Morgyn S <125914137+stryker-sk@users.noreply.github.com>
2023-02-26 22:37:38 -0800

    Create models.py

---
+++ src/utils/models.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.db import models
+
+
+class AuditModelMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='%(app_label)s_%(class)s_created_by',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='%(app_label)s_%(class)s_updated_by',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+    )
+    
+    class Meta:
+        abstract = True
