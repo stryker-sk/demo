03830129a0658475104b13b03a77e9428e55eb3f
Morgyn S <125914137+stryker-sk@users.noreply.github.com>
2023-02-26 22:13:50 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,3 +7,10 @@ logger = logging.getLogger(__name__)
 def hello_world(name: str):
     logger.debug('Input name: %s', name)
     print(f'Hello {name}')
+
+
+def read_json(file_name: str):
+    with open(file_name) as f:
+        data = json.load(f)
+    
+    print(data)
