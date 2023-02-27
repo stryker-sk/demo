5edd76058940e55c9d921a893d0bd003e9c63d2a
Morgyn S <125914137+stryker-sk@users.noreply.github.com>
2023-02-26 22:10:12 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 # demo script for GitHub Actions
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def hello_world(name: str):
+    logger.debug('Input name: %s', name)
     print(f'Hello {name}')
