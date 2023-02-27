1738534bc2dc54a19dea4aeb660ab16338c71f62
Morgyn S <125914137+stryker-sk@users.noreply.github.com>
2023-02-26 22:32:59 -0800

    Update main.py (#1)
    * Update main.py
    
    * Update main.py
    
    * Update flake8.yml
    
    * Update main.py
    
    * Update main.py
    
    * Update flake8.yml

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
 # demo script for GitHub Actions
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def hello_world(name: str):
+    """Trigger GHA change"""
+    logger.debug('Input name: %s', name)
     print(f'Hello {name}')
+
+
+def read_json(file_name: str):
+    with open(file_name) as f:
+        data = json.load(f)
+    
+    print(data)
+
+
+def foo_bar():
+    logger.error('You have been foo bar')
